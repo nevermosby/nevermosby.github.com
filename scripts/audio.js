a54a33340a44da9e65be6cba0313a9ee94e232b8
nevermosby <robolwq@qq.com>
2012-10-02 16:17:40 +0800

    add my loveyi website

---
+++ scripts/audio.js
@@ -0,0 +1,325 @@
+﻿
+    String.prototype.trim = function () {
+        return this.replace(/(^\s*)|(\s*$)/g, "");
+    }
+    var audio = new Audio();
+   
+    var songs = new Array( 
+                            "没那么简单.mp3",
+                            "leave out all the rest.mp3",
+                             "笔记.mp3",
+							 "the scientist.mp3",
+							 "做你的男人"
+                             //"浮夸.mp3",
+                             //"老男孩.mp3"
+                             //"那就这样吧.mp3"
+                             );
+    var songsurl = new Array(
+                                "http://www.aaron.cn/mp3/mei-jiandan.mp3",
+                                "http://stevenessani.com/leave.out.all.the.rest.mp3",
+                                "http://zxdj.jnu.edu.cn/ystk/yinyue/%E7%AC%94%E8%AE%B0.mp3",
+								"https://dl.dropbox.com/u/18794215/mp3/The%20Scientist.mp3",
+								"http://zhangmenshiting2.baidu.com/data2/music/1169923/1169923.mp3"
+
+    //"leave out all the rest.mp3", "笔记.mp3"
+                                );
+    var songdir = "mp3/";
+    var songtmp = "";
+    var sungsong = new Array(songs.length);
+
+    for (var i = 0; i < songs.length; i++) {
+        sungsong[i] = false;
+    }
+
+    var sungcount = 0;
+    var second = 0;
+    var time;
+
+    //for nextsong()
+    var randomnum;
+    var alltrue = false;
+   // var rest = true;
+
+
+    //set second plus
+    function timeCount() {
+        //var st = document.getElementById("t1");
+       // st.value=second;
+        second = second + 1;
+        time = setTimeout("timeCount()",1000);
+    }
+    //pause timeout
+    function pauseCount() {
+        clearTimeout(time);
+    }
+
+
+    //songs[0] = "";
+    //var pause = document.getElementById("pause");
+        function Song() {
+        this.name = "";
+        this.author = "";
+    }
+    var sg = new Song();
+
+    
+    function showNext(){
+
+        //var next = document.getElementById("next");
+        var before = document.getElementById("before");
+
+        before.innerHTML= $("#next").offset().top+", "+$("#next").offset().height;
+
+        // alert(next.style.top);
+    }
+
+	//judge useragent
+	function getUseragent(){
+	
+	
+		var ua =window.navigator.userAgent.toLowerCase();//windows.navigator.userAgent.toLowerCase();
+		var uasp = document.getElementById("ua");
+		if(ua.indexOf("msie")>=1)
+		{
+			uasp.innerHTML="你用的是IE吧,使用IE9浏览，效果最好哦~"
+		}
+		if(ua.indexOf("firefox")>=1)
+		{
+			uasp.innerHTML="不好意思,对FF支持还不完善，试试IE9或者chrome stable吧~"
+		}
+		if(ua.indexOf("chrome")>=1)
+		{
+			uasp.innerHTML="你用的是chrome吧，good choice~"
+		}
+		
+		//$("#ua").innerHTML=ua;
+		
+	}
+
+    $(document).ready(init);
+    // window.onload = init();
+
+    function display() {
+   
+        $("#next").next("em").animate({ opacity: "show", top: $("#next").offset().top-75 }, "slow");
+    }
+
+    function disappear() {
+        $("#next").next("em").animate({ opacity: "hide", top: $("#next").offset().top-75 }, "fast");
+    }
+    function init() {
+
+        //display();
+        //showNext();
+		getUseragent();
+        setInterval(displayTime, 1000);
+        playsongs();
+        
+    }
+    function displayTime(){
+
+        var time = document.getElementById("time");
+        var s = parseInt(audio.currentTime % 60);
+        var m = parseInt((audio.currentTime / 60) % 60);
+        time.innerHTML = m + '.' + s ;
+
+        var duration = document.getElementById("duration");
+
+        var dm = parseInt((audio.duration / 60) % 60);
+        var ds = parseInt(parseInt(audio.duration) - dm * 60);
+        duration.innerHTML = dm + ':' + ds;
+
+        if (audio.ended) {
+            alert("click to next song~");
+            NextSong();
+        }
+        if (s == 10 || s==30 ) {
+            display();
+        }
+        if (s == 15 || s== 35) {
+            disappear();
+        }
+       //try to use the percentage of song
+    }
+
+    function playsongs() {
+
+        //alert(songsurl.length);
+       document.getElementById("songname").focus();
+       document.getElementById("songname").select();
+
+       //for audio to play the song
+        var songurl = "";
+        var randomsongnum = randomNum();
+        //alert(randomsongnum);
+        sungsong[randomsongnum] = true;
+        pretoplay(randomsongnum);
+      // songurl = songdir + songs[randomsongnum];
+       //alert(songurl);
+      // setsong(songurl);
+        // songtmp = returnSongname(songs[randomsongnum]);
+        alert("Start Now");
+
+/*
+        var a2 = new Audio();
+        a2.setAttribute("src", "http://www.aaron.cn/mp3/mei-jiandan.mp3");
+        a2.play();
+*/
+        timeCount();
+        playp();
+
+        
+
+   }
+   //papare a song to play
+   function pretoplay(num) {
+
+       songurl = songsurl[num];  //songdir + songs[num];
+       //alert(num);
+       setsong(songurl);
+       songtmp = returnSongname(songs[num]);
+
+   }
+   //give a random number
+   function randomNum() {
+       return Math.floor(Math.random() * songs.length);
+   }
+
+    /*
+    next songs to play\
+    */
+   function NextSong() {
+
+       var rest = true;
+       /*
+        var randomnum;
+        var alltrue = false;
+        var rest = true;
+        */
+        if (alltrue) {
+
+
+            pausep();
+
+            alert("u have reached through the song list~~");
+            //alert("you may click start to restart this game");
+            //make it auto restart
+            if (confirm("r u going to restart it and have another try?")) {
+
+                for (var i = 0; i < sungsong.length; i++) {
+                    sungsong[i] = false;
+                }
+                alltrue = false;
+
+                restart();
+            }
+            else {
+            //need to check 
+                document.write("Thank u for playing, plz wait to play new mode~");
+            }
+
+        }
+        
+        
+    //random out a fresh num
+    do {
+        randomnum = randomNum();
+    }
+    while (sungsong[randomnum]);
+
+    sungsong[randomnum] = true;
+    
+    //判断是不是所有标记都是true了。
+
+    for (var i in sungsong)
+    {
+       
+       rest = rest && sungsong[i];
+
+   }
+  // alert(rest);
+   alltrue = rest;
+   //alert(alltrue);
+    
+    pretoplay(randomnum);
+    playp();
+
+    }
+
+    function restart() {
+
+       
+        NextSong();
+
+    }
+
+    //validate the song
+    function validate() {
+        audio.pause();
+        var usersong = document.getElementById("songname");
+        //usersong.trim();
+        //alert("user:" + usersong.value.trim() + ",thisSong: " + songtmp);
+        var flag = false;
+
+        if (usersong.value.trim() == songtmp) {
+                sungcount++;
+                alert("Correct,click the confirm to play the next song");
+                if (sungcount != songsurl.length) {
+                   
+                    NextSong();
+                } 
+            }
+            else {
+                alert("Wrong");
+                playp(); //audio.play();
+               
+            }
+ 
+    }
+    //watch the enter code
+    function enterpress() {
+        if (event.keyCode == 13) {
+           
+            validate();
+           // alert("sungcount: " + sungcount);
+            if (sungcount == songsurl.length) {
+                alert("that is the last song.The end~");
+                alert("you have consumed "+second+" second!");
+                document.write("Thank u for playing, plz wait for us to play new mode~");
+            }
+           // else {
+           //     validate();
+           // }
+            //alert("entered");
+        }
+    }
+   // function setFocus
+   //return the name of song only
+    function returnSongname(extendedsong) {
+
+       var retSong=
+        extendedsong.substring(0, extendedsong.indexOf("."));
+        return retSong;
+    }
+    function playp(){
+        // alert("play");
+       document.getElementById("songname").focus();
+       document.getElementById("songname").select();
+        audio.play();
+    }
+
+    function pausep() {
+        //alert("pause");
+        audio.pause();
+    }
+
+    function setsong(songurl) {
+        //var aurl = document.getElementById("audiofile");
+        audio.setAttribute("src", songurl);
+    }
+
+    function alertu() {
+
+        window.open("http://weibo.com/robolwq");
+
+    }
---
+++ scripts/functions.js
@@ -0,0 +1,322 @@
+﻿var $window=$(window),gardenCtx,gardenCanvas,$garden,garden;
+$(function()
+{
+	$loveHeart=$("#loveHeart");
+	var a=$loveHeart.width()/2;
+	var b=$loveHeart.height()/2-55;
+	$garden=$("#garden");
+	gardenCanvas=$garden[0];
+	gardenCanvas.width=$("#loveHeart").width();
+	gardenCanvas.height=$("#loveHeart").height();
+	gardenCtx=gardenCanvas.getContext("2d");
+	gardenCtx.globalCompositeOperation="lighter";
+	garden=new Garden(gardenCtx,gardenCanvas);
+	$("#content").css("width",$loveHeart.width()+$("#code").width());
+	$("#content").css("height",Math.max($loveHeart.height(),$("#code").height()));
+	$("#content").css("margin-top",Math.max(($window.height()-$("#content").height())/2,10));
+	$("#content").css("margin-left",Math.max(($window.width()-$("#content").width())/2,10));
+	setInterval(function(){garden.render()},Garden.options.growSpeed)
+});
+	$(window).resize(function(){location.replace(location)});
+	function getHeartPoint(c)
+	{
+		var b=c/Math.PI;
+		var a=19.5*(16*Math.pow(Math.sin(b),3));
+		var d=-20*(13*Math.cos(b)-5*Math.cos(2*b)-2*Math.cos(3*b)-Math.cos(4*b));
+		return new Array(offsetX+a,offsetY+d)
+	}
+	function startHeartAnimation()
+	{
+		var c=50;
+		var d=10;
+		var b=new Array();
+		var a=setInterval(function()
+						 {
+							var h=getHeartPoint(d);
+							var e=true;
+							for(var f=0;f<b.length;f++)
+							{
+								var g=b[f];
+								var j=Math.sqrt(Math.pow(g[0]-h[0],2)+Math.pow(g[1]-h[1],2));
+								if(j<Garden.options.bloomRadius.max*1.3)
+									{
+										e=false;
+										break
+									}
+							}
+							if(e)
+							{
+								b.push(h);
+								garden.createRandomBloom(h[0],h[1])
+							}
+							if(d>=30)
+							{
+								clearInterval(a);
+								showMessages()
+							}
+							else
+							{
+								d+=0.2
+							}
+						},c)
+	}
+	(	function($)
+		//扩展jQuery的函数，即$("id").youraddfunction();
+		{
+			//jQuery.fn = jQuery.prototype
+			//the following code is add function-- typewriter
+			$.fn.typewriter=function(callback)
+				{
+					//each（）对于每个匹配元素都要执行的函数
+					//匹配条件是什么？
+					//这里的this是jQuery的还是Dom的？	
+					//应该是指每个div下的元素					
+					this.each(function()
+					{
+						var d=$(this),
+						c=d.html(),
+						// '<span class=\"d3\">Dear Yi,<br/>Since we met, it\'s been <br/></span><span id=\"metdays\" class=\"dayD3\"></span><span class=\"d3\"><br/>Days.<br/>I am always ur only -1~<br/></span><span id=\"video100\"></span>',
+				
+						b=0;
+						//d is the div named by id
+						//c is the first element in d
+							//restore element in div
+						//document.writeln("d in each func:"+d);
+						//document.writeln("c in each function:"+c);
+						//clear the div
+                        //console.log(c);
+						d.html("");
+						var e=setInterval(function()
+											{
+												//js:substr(start,length)
+												var f=c.substr(b,1);
+												//document.writeln("f:"+f);
+												if(f=="<")
+												{
+													b=c.indexOf(">",b)+1
+												}
+												else
+												{
+													b++
+												}
+												//js:substring(start,stop)
+												//b&1，位And运算，每隔一个显示。
+								d.html(c.substring(0,b));
+								//+(b&1?"<span class=d3>_</span":""));
+												if(b>=c.length)
+												{
+													clearInterval(e);
+													callback();
+												}
+											},75)
+					});
+					return this;
+				}
+		}
+	)(jQuery);
+	
+	
+	function timeElapse(c)
+	{
+	var e=Date();
+	var f=(Date.parse(e)-Date.parse(c))/1000;
+	var g=Math.floor(f/(3600*24));
+	f=f%(3600*24);
+	var b=Math.floor(f/3600);
+	if(b<10){b="0"+b}f=f%3600;
+	var d=Math.floor(f/60);
+	if(d<10){d="0"+d}f=f%60;if(f<10){f="0"+f}
+	var a='<span id ="days" class="digit">'+g+'</span> days <span class="digit">'+b+'</span> hours <span class="digit">'+d+'</span> minutes <span class="digit">'+f+"</span> seconds";
+	$("#elapseClock").html(a);
+	//alert($("#days").val());
+//    //fireout
+//	if (Math.abs(g - 100) < 15)
+//	    onFire();
+//        
+	}
+
+    //for the time countdown of meeting
+	function metTimeElapse(met) {
+
+	    var now = Date();
+	    var s = (Date.parse(now) - Date.parse(met)) / 1000;
+	    var d = Math.floor(s / (3600 * 24));
+
+	    //var days = '<span>' + d + '</span>';
+	    $("#metdays").html(d);
+
+    }
+
+
+	//called by startHeartAnimation
+	//after the HeartAnimation
+	function showMessages()
+	{
+		adjustWordsPosition();
+		$("#messages").fadeIn(3000,function()
+			{
+				showLoveU(imgSlider);
+			});
+		//$("#messages").addClass("d3");
+	}
+	
+	function adjustWordsPosition()
+	{
+		$("#words").css("position","absolute");
+		$("#words").css("top",$("#garden").position().top+195);
+		$("#words").css("left",$("#garden").position().left+70)
+	}
+	function adjustCodePosition()
+	{
+		$("#code").css("margin-top",($("#garden").height()-$("#code").height())/2)
+	}
+	function showLoveU(callback)
+	{
+		$("#loveu").fadeIn(2000,callback);
+		//callback();
+	}
+	
+	
+	//judge useragent
+	function getUseragent(){
+	
+	
+		var ua =window.navigator.userAgent.toLowerCase();//windows.navigator.userAgent.toLowerCase();
+		var uasp = document.getElementById("ua");
+		if(ua.indexOf("msie")>=1)
+		{
+			uasp.innerHTML="你用的是IE吧,目前IE还看不到绚丽的效果哦，赶快获得最主流的浏览器吧~";
+			var createDiv = document.createElement("div");
+			createDiv.innerHTML ="<a href=&quot;http://www.google.com/chrome/&quot;>click me</a>";
+			uasp.appendChild(createDiv);
+		}
+		if(ua.indexOf("firefox")>=1)
+		{
+			if(ua.indexOf("10")>=1)
+			{
+				uasp.innerHTML=
+				"WOW，您是FF10的使用者，good choice";
+			}
+			else{
+				uasp.innerHTML="你用是FF的旧版本，请升级最新版，获得最佳体验哦～";
+			}
+		}
+		if(ua.indexOf("chrome")>=1)
+		{
+			uasp.innerHTML="你用的是chrome吧，good choice~";
+		}
+
+		$("#ua").fadeOut(5000);
+
+}
+
+
+//fire out the firework
+function onFire() {
+
+
+    //big circle
+    var big = setInterval('createFirework(99, 150, 8, 7, null, null, null, null, false, true)', 1500);
+    //central
+    var central = setInterval('createFirework(25, 187, 5, 1, null, null, null, null, false, true)', 1000);
+    //standard
+    var standard = setInterval('createFirework(11, 30, 3, 4, null, null, null, null, false, true)', 2000);
+
+    setTimeout(function () {
+        clearInterval(big);
+        clearInterval(central);
+        clearInterval(standard);
+    }, 15000);
+}
+
+//image view function
+function imgSlider()
+{
+	var words = $('#words');
+	var roseDiv = $('#code');
+	var roseCanvas = $('#rose');
+	//make this image silder on the place of heartgarden
+	//first, make garden canvas hidden
+	console.log('enter imgSlider');
+	// $('#garden').animate({
+	// 		display: none	
+	// 	},500,function()
+	// {
+	// 	console.log('completed hide');
+	// });
+	$('#garden').fadeOut(500,function(){
+		$('#loveu').fadeOut(500);
+		console.log('completed hide');
+		words.animate({
+
+			'top':'-=230px'
+		},2000,'swing',function(){
+
+			console.log('word top done');
+			//rose fade out
+			//TODO: come to center then fade out
+			roseDiv.animate({
+				//no effect show
+				//maybe canvas problem
+				'left':'+=100px'
+			},1500,'swing',function(){
+
+				console.log('completed center rose');
+				// change the content of #garden for the anniversary of fall-in-love day.
+				var fdays = $('#days').html(); 
+				console.log('fall-in-love days: ' + fdays);
+				if(fdays%365==0)
+				{
+					$(this).fadeOut(2500, function(){
+						// change the content
+						var html = '<span class=\"anni3\">Today is our anniversary, let us recall the good times~<br/></span>';
+                		console.log("anniversary html: " + html);
+						$('#loveu').html(html);
+						$('#loveu').fadeIn(1500, function()
+							{
+								onFire();
+								showIframe();
+							});
+					});
+				}
+				else
+				{
+					$(this).fadeOut(1500,function(){
+						showIframe();
+					});
+
+				}
+				// iframeDiv.css("position","absolute");
+				// iframeDiv.css("top",words.position().top+words.height()+10);
+				// iframeDiv.css("left",words.position().left-(ifWidth)/2)
+				// iframeDiv.fadeIn(2500);
+			});
+
+			//just fade out
+			// $('#code').fadeOut(1000,function(){
+
+			// //make image slider
+			// iframeDiv.css("position","absolute");
+			// iframeDiv.css("top",words.position().top+words.height()+10);
+			// iframeDiv.css("left",words.position().left-(ifWidth)/2)
+			// iframeDiv.fadeIn(1000);
+			// });			
+			//bind keyboard
+			
+		});
+	});
+}
+
+function showIframe()
+{
+	var words = $('#words');
+	var iframeDiv = $('#ifDiv');
+	var ifWidth = iframeDiv.width();
+	iframeDiv.css("position","absolute");
+	iframeDiv.css("top",words.position().top+words.height()+10);
+	iframeDiv.css("left",words.position().left-(ifWidth)/2)
+	iframeDiv.fadeIn(1500);
+}
+
+//function bindKeyBoard()
+	
